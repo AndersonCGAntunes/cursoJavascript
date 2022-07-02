24609b46578a44ad822d0a070291a5ed73de4eb8
AndersonClayton <andersonclaytondesenvolvedor@gmail.com>
2022-07-02 17:21:27 -0300

    Commit dos primeiros códigos em JavaScript

---
+++ Basic_JavaScript/scripts.js
@@ -0,0 +1,198 @@
+window.onload = function() {
+    //const myStr = "I am a \"double quoted\" string inside \"double quotes\".";
+
+    //const myStr = '<a href="http://www.example.com" target="_blank">Link</a>';
+
+    //const myStr = "FirstLine\n\t\\SecondLine\nThirdLine";
+
+    //const myStr = "This is the start." + " This is the end.";
+
+    /*let myStr = "This is the first sentence. ";
+    myStr += "This is the second sentence.";*/
+
+    /*const myName = "Anderson ";
+    const myStr = "My name is " + myName + "and I am well!";*/
+
+    /*const someAdjective = "cool!";
+    let myStr = "Learning to code is ";
+    myStr += someAdjective;*/
+
+    /*let lastNameLength = 0;
+    const lastName = "Lovelace";
+    // Only change code below this line
+    lastNameLength = lastName.length;*/
+
+    /*
+    // Setup
+    let firstLetterOfLastName = "";
+    const lastName = "Lovelace";
+    // Only change code below this line
+    firstLetterOfLastName = lastName[0]; // Change this line
+    */
+
+    /*
+    // Setup
+    let myStr = "Jello World";
+    // Only change code below this line
+    myStr = "Hello World"; // Change this line
+    // Only change code above this line
+    */
+
+    /*
+    // Setup
+    const lastName = "Lovelace";
+    // Only change code below this line
+    const thirdLetterOfLastName = lastName[2]; // Change this line
+    */
+
+    /*
+    // Setup
+    const lastName = "Lovelace";
+    // Only change code below this line
+    const lastLetterOfLastName = lastName[lastName.length - 1]; // Change this line
+    */
+
+    /*
+    // Setup
+    const lastName = "Lovelace";
+    // Only change code below this line
+    const secondToLastLetterOfLastName = lastName[lastName.length - 2]; // Change this line
+    */
+
+    /*
+    const myNoun = "dog";
+    const myAdjective = "big";
+    const myVerb = "ran";
+    const myAdverb = "quickly";
+
+    // Only change code below this line
+    const wordBlanks = "My " + myNoun + " is so " + myAdjective + " and " + myVerb + " very " + myAdverb; // Change this line
+    // Only change code above this line
+    */
+
+    /*
+    // Only change code below this line
+    const myArray = ["Apple", 36];
+    */
+
+    /*
+    // Only change code below this line
+    const myArray = [[21, 25], [32,44]];
+    */
+
+    /*
+    const myArray = [50, 60, 70];
+
+    let myData = myArray[0];
+    */
+
+    /*
+    // Setup
+    const myArray = [18, 64, 99];
+
+    // Only change code below this line
+    myArray[0] = 45;
+    */
+
+    /* BUSCAR ELEMENTOS NO ARRAY
+    const myArray = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+    [[10, 11, 12], 13, 14],
+    ];
+
+    const myData = myArray[2][1];
+    */
+
+    /*
+    // ADICIONAR UM ITEM A MAIS NO ARRAY
+    const myArray = [["John", 23], ["cat", 2]];
+
+    // Only change code below this line
+    myArray.push(["dog", 3]);
+    */
+
+    /*
+    // RETIRAR PRIMEIRO ITEM DO ARRAY
+    const myArray = [["John", 23], ["cat", 2]];
+
+    // Only change code below this line
+    let removedFromMyArray = myArray.pop();
+    */
+
+    /*
+    // RETIRAR ÚLTIMO ITEM DO ARRAY
+    const myArray = [["John", 23], ["dog", 3]];
+
+    // Only change code below this line
+    let removedFromMyArray = myArray.shift();
+    */
+
+    /*
+    
+    */
+
+    /*
+    
+    */
+
+    /*
+    
+    */
+
+    /*
+    
+    */
+
+    /*
+    
+    */
+
+    /*
+    
+    */
+
+    /*
+    
+    */
+
+    /*
+    
+    */
+
+    /*
+    
+    */
+
+    /*
+    
+    */
+
+    /*
+    
+    */
+
+    /*
+    
+    */
+
+    /*
+    
+    */
+
+    /*
+    
+    */
+
+    /*
+    
+    */
+
+    /*
+    
+    */
+
+    document.getElementById('paragrafo').innerHTML = myStr;
+    console.log(myStr);
+}
